5d9d75fae8393b928b2815e9ef84e5d657ca3803
devin-bielejec <devin.bieleje@gmail.com>
2020-01-09 16:05:57 -0500

    Added routes and nav to app file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Router, Route } from "react-router-dom";
+/* 
+Nav:
 
+About
+-summary, some other tid bits
+Project
+-liveinthebestplace
+-disneyparent
+-mesofunny
+Contact
+-github
+-twitter
+-linkedin
+
+Centered page
+
+
+*/
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Nav />
+      <Route path="/about" component={About} />
+      <Route path="/projects" component={Projects} />
+      <Route path="/contact" component={Contact} />
+    </Router>
   );
 }
 
