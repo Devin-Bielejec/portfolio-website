6b67233bb6ac8b8b0083a47f2263f9702e83428a
devin-bielejec <devin.bieleje@gmail.com>
2020-01-10 14:55:18 -0500

    Updated

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,26 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+const StyledNav = styled.nav`
+  display: flex;
+  justify-content: space-around;
+  font-size: 2rem;
+  margin: 10px 0;
+`;
+
+const StyledNavLink = styled(NavLink)`
+  text-decoration: none;
+  color: black;
+`;
 
 const Nav = () => {
   return (
-    <nav>
-      <NavLink to="/about">About</NavLink>
-      <NavLink to="/projects">Projects</NavLink>
-      <NavLink to="/contact">Contact</NavLink>
-    </nav>
+    <StyledNav>
+      <StyledNavLink to="/about">About</StyledNavLink>
+      <StyledNavLink to="/projects">Projects</StyledNavLink>
+      <StyledNavLink to="/contact">Contact</StyledNavLink>
+    </StyledNav>
   );
 };
 
